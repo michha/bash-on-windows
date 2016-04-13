cebd2179b5c4eac94e73312078652fc30aaa1fee
michha <michha@users.noreply.github.com>
2016-04-13 18:17:31 +0200

    Update cakephp-setup.sh

--- cakephp-setup.sh
+++ cakephp-setup.sh
@@ -1,10 +1,13 @@
+echo "Hello $USER"
+
 echo "Setting up cakephp environment"
 
+echo "Updating packaging tool..."
 apt-get update
 apt-get upgrade
 
 echo "Installing php packages..."
 apt-get install -y php5 php5-mysql php5-cli php5-intl php5-sqlite
 
-echo "Installing git"
+echo "Installing git package..."
 apt-get install -y git
