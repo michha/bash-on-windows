f7c5ae7e284e773047e7a8cd2aa2a108168ac543
michha <michha@users.noreply.github.com>
2016-04-26 16:38:13 +0200

    Update cakephp-setup.sh
    sudo required in latest Bash On Windows version

--- cakephp-setup.sh
+++ cakephp-setup.sh
@@ -9,11 +9,11 @@ echo "Hello $USER"
 echo "Setting up cakephp environment"
 
 echo "Updating packaging tool..."
-apt-get update
-apt-get upgrade
+sudo apt-get update
+sudo apt-get upgrade
 
 echo "Installing php packages..."
-apt-get install -y php5 php5-mysql php5-cli php5-intl php5-sqlite
+sudo apt-get install -y php5 php5-mysql php5-cli php5-intl php5-sqlite
 
 echo "Installing git package..."
-apt-get install -y git
+sudo apt-get install -y git
