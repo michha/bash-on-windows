5659fe4f3dd71b353e8c94698d23e0c09aa58d7a
michha <michha@users.noreply.github.com>
2016-05-05 17:16:56 +0200

    Update ruby-setup.sh

--- ruby-setup.sh
+++ ruby-setup.sh
@@ -1,3 +1,9 @@
+#!/bin/bash
+# Exit the script as soon as an executed command does not succeed
+set -o errexit
+# Exit the script when an unintialized variable is used
+set -o nounset
+
 sudo apt-add-repository ppa:brightbox/ruby-ng
 sudo apt-get update
 sudo apt-get -y install ruby2.3 ruby2.3-dev ruby-switch
