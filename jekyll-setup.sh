e85da5f7c5dc3ddbd46f513d05bd1093aa3fd0be
Michael <michha@users.noreply.github.com>
2016-05-18 17:45:44 +0200

    Update jekyll-setup.sh

--- jekyll-setup.sh
+++ jekyll-setup.sh
@@ -1,3 +1,9 @@
+#!/bin/bash
+# Exit the script as soon as an executed command does not succeed
+set -o errexit
+# Exit the script when an unintialized variable is used
+set -o nounset
+
 #first setup ruby
 curl -s -L https://github.com/michha/cakephp-windows10-bash/raw/master/ruby-setup.sh | sh
 
